cca02b7a34641c6ff2eac263963fe6be176ebaf3
Julianzeng <zenghj@outlook.com>
2018-07-26 10:45:54 +0800

    style fix

--- web/src/apps/admin/components/MyHeader.vue
+++ web/src/apps/admin/components/MyHeader.vue
@@ -1,15 +1,18 @@
 <template>
-  <el-menu class="my-header" mode="horizontal">
-    <el-menu-item index="1">
-      <router-link :to="{name: 'articles'}">在线文章</router-link>
-    </el-menu-item>
-    <el-menu-item  index="2">
-      <router-link :to="{name: 'draftArticles'}">草稿箱</router-link>
-    </el-menu-item>
-    <el-menu-item  index="3">
-      <router-link :to="{name: 'editArticle'}">发布新文章</router-link>
-    </el-menu-item>
-  </el-menu>
+  <header id="admin-header">
+    <el-menu mode="horizontal">
+      <el-menu-item index="1">
+        <router-link :to="{name: 'articles'}">在线文章</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <router-link :to="{name: 'draftArticles'}">草稿箱</router-link>
+      </el-menu-item>
+      <el-menu-item index="3">
+        <router-link :to="{name: 'editArticle'}">发布新文章</router-link>
+      </el-menu-item>
+    </el-menu>
+  </header>
+
 </template>
 <script>
   export default {
@@ -22,7 +25,7 @@
 </script>
 
 <style lang="less">
-.my-header {
+#admin-header {
   margin-bottom: 1em;
   .el-menu-item {
     height: 3em;
